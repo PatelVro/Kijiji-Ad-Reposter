2e8f989dfd819bd48b4b7fd80307829ce1a30655
Hill Patel <86945160+PatelVro@users.noreply.github.com>
2025-04-26 17:52:46 -0400

    Update kjj_auto_with_excel-Linux.py

--- kjj_auto_with_excel-Linux.py
+++ kjj_auto_with_excel-Linux.py
@@ -7,6 +7,7 @@ import pymysql
 import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
